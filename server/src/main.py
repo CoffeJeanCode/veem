579b38e95cc056f05c8333d44058231786e19410
Jean Pierre Ortiz <ortizmurciajeanpierre@gmail.com>
2024-08-22 23:11:33 -0500

    add cors for client conection

--- server/src/main.py
+++ server/src/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from src.config.database import engine, metadata
 from src.routes.user import user
+from src.routes.auth import auth
 
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user)
+app.include_router(auth)
